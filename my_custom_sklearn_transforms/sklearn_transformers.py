92b649a3b0cfd20b1ec9dc1ce835861ca5e34359
agloks <smoke.day@yahoo.com>
2020-08-21 17:46:41 -0300

    first up

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -12,5 +12,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        not_have_dificulty_exatas = data[(data["PERFIL"] == "EXATAS") & (data["NOTA_MF"] > 6)].index
+        not_have_dificulty_all = data[(data["PERFIL"] == "DIFICULDADE") & \
+        ( (data["NOTA_DE"] > 6) \
+        | (data["NOTA_EM"] > 6) ) & \
+        ( (data["NOTA_MF"] > 6) \
+        | (data["NOTA_GO"] > 6) )].index
+        
+        indexes = not_have_dificulty_all.append(not_have_dificulty_exatas)
+        data = data.drop(index = not_have_dificulty_all)
+
+        data["BOM_ALUNO"] = data["NOTA_MF"].apply(lambda x: 1 if  x > 8 else 0)
+        data["PESSIMO_ALUNO"] = data["NOTA_MF"].apply(lambda x: 1 if  x < 3 else 0)
+        data["MUITO_BOM_GUYS"] = data["NOTA_MF"].apply(lambda x: 1 if x >= 7 and x <= 12 else 0)
+
+        data = data.dropna(inplace=False)
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
